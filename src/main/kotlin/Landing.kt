f716bb2c523b4571cbed6a267114d0c6328228f9
James Tapsell <email@jrtapsell.co.uk>
2018-01-16 17:20:14 +0000

    Added URL logic

--- src/main/kotlin/Landing.kt
+++ src/main/kotlin/Landing.kt
@@ -1,6 +1,13 @@
 import kotlin.browser.window
 
+val safeRegex = Regex("[0-9.-A-Za-z]+")
+
 @JsName("start")
 fun start() {
-    Controller.loadURL(window.location.search.substring(3))
+    val url = window.location.pathname.split("/").last()
+    if (safeRegex.matchEntire(url) == null) {
+        window.alert("The url may be mallicious")
+    } else {
+        Controller.loadURL(window.location.search.substring(3))
+    }
 }
